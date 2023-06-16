f269a2287c3aa5e4da8e7f6f72eb8824b884abee
tristan <Tristan_brown_1@hotmail.co.uk>
2023-06-16 15:24:42 +0100

    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-
+import words 
 stages = ['''
   +---+
   |   |
@@ -57,9 +57,8 @@ stages = ['''
 =========
 ''']
 
-word_list = ['next']
 lifes = 6
-choosen_word = random.choice(word_list) 
+choosen_word = random.choice(words.word_list) 
 display = []
 blank = '_'
 end_of_game = False
@@ -71,23 +70,29 @@ print(display)
 
 while end_of_game == False:
     choosen_letter = input("please choose letter:")
+
+    if choosen_letter in display:
+        print('you have already guess this letter')
+
     for i in range(len(choosen_word)):
         letter = choosen_word[i]
         if letter in  choosen_letter:
             print("you were correct")
             display[i] = choosen_letter
             print(display)
-    
+   
     if choosen_letter not in choosen_word:
             lifes -= 1
             #stages[9] -= stages[0]
             print("you where wrong")
             print(stages[lifes])
 
+
+
     if blank not in display[:]:
           end_of_game = True
           print('you have won')
-          
+
     if lifes == 0: 
         print('you lose')
         end_of_game = True
