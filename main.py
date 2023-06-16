effd49b00bdb6a5050b9e31043b68305e9aedbc8
tristan <Tristan_brown_1@hotmail.co.uk>
2023-06-16 15:08:25 +0100

    update

---
+++ main.py
@@ -0,0 +1,103 @@
+import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+word_list = ['next']
+lifes = 6
+choosen_word = random.choice(word_list) 
+display = []
+blank = '_'
+end_of_game = False
+
+for i in range(len(choosen_word)): 
+        display  += '_'
+
+print(display)
+
+while end_of_game == False:
+    choosen_letter = input("please choose letter:")
+    for i in range(len(choosen_word)):
+        letter = choosen_word[i]
+        if letter in  choosen_letter:
+            print("you were correct")
+            display[i] = choosen_letter
+            print(display)
+    
+    if choosen_letter not in choosen_word:
+            lifes -= 1
+            #stages[9] -= stages[0]
+            print("you where wrong")
+            print(stages[lifes])
+
+    if blank not in display[:]:
+          end_of_game = True
+          print('you have won')
+          
+    if lifes == 0: 
+        print('you lose')
+        end_of_game = True
+          
+
+
+
+'''for i in range(len(choosen_word)):
+     if display[1] in display:
+        display.pop()
+        print(display)'''
+#for i in range(len(choosen_word)): 
+#        blank  += '_'
